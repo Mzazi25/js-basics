f802df5a8d08220821e53714cee04031617aedc0
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2021-12-14 20:35:59 +0300

    added Male and Female Akan Names to JS

--- index.js
+++ index.js
@@ -10,24 +10,56 @@ var dayInput = document.getElementById("day").value;
 var monthInput = document.getElementById("month").value;
 var yearInput = document.getElementById("year").value;
 
-document.getElementById("myform").addEventListener("submit", akanNames);
+form.addEventListener("submit", akanNames);
 
-function akanNames(){
+function akanNames(e){
+
+    e.preventDefault();
+
     let CC = parseInt(yearInput.subSting(0,2));
     let YY = parseInt(yearInput.subSting(2,4));
     let dayOfWeek= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(monthInput+1)/10)) + dayInput ) % 7;
 
 
-    if(dayInput <=0 || dayInput>31){
-        alert("Enter Valid Date")
+    if(dayInput===""){
+        alert("Please input the day")
+
+    }else if (dayInput<=0 || dayInput>31){
+        alert("Enter Valid Day")
 
     }
 
-    if(monthInput<=0 || monthInput>12){
-        alert("Enter a Valid Month")
-    }
-
+    if(monthInput ===""){ 
+    alert ("Please input the day")
+    }else if (monthInput >12){
+        alert("Please Enter a valid Day")
     
+    }
+    // Male Akan Names
+
+    if (document.getElementById("check").Male & dayOfWeek[0] ){
+        alert("Hey, your Akan name is Kwasi")
+
+    }
+    if (document.getElementById("check").Male & dayOfWeek[1] ){
+        alert("Hey, your Akan name is Kwadwo")
+    }
+    if (document.getElementById("check").Male & dayOfWeek[2] ){
+        alert("Hey, your Akan name is Kwabena")
+    }
+    if (document.getElementById("check").Male & dayOfWeek[4] ){
+        alert("Hey, your Akan name is Kwaku")
+    }
+    if (document.getElementById("check").Male & dayOfWeek[5] ){
+        alert("Hey, your Akan name is Yaw")
+    }
+    if (document.getElementById("check").Male & dayOfWeek[6] ){
+        alert("Hey, your Akan name is Kofi")
+    }
+    if (document.getElementById("check").Male & dayOfWeek[6] ){
+        alert("Hey, your Akan name is Kwame")
+    }
+
 
 
 
