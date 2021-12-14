9ab703a67011af11f5192f688191d4ad86e8ecfd
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2021-12-14 16:01:33 +0300

    Added if statement for Invalid dates and Months

--- index.js
+++ index.js
@@ -5,10 +5,33 @@ let form = document.getElementById("myform");
 let Male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
 let Female = [ "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+
+var dayInput = document.getElementById("day").value;
+var monthInput = document.getElementById("month").value;
+var yearInput = document.getElementById("year").value;
+
 document.getElementById("myform").addEventListener("submit", akanNames);
 
 function akanNames(){
+    let CC = parseInt(yearInput.subSting(0,2));
+    let YY = parseInt(yearInput.subSting(2,4));
+    let dayOfWeek= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(monthInput+1)/10)) + dayInput ) % 7;
+
+
+    if(dayInput <=0 || dayInput>31){
+        alert("Enter Valid Date")
+
+    }
+
+    if(monthInput<=0 || monthInput>12){
+        alert("Enter a Valid Month")
+    }
+
     
+
+
+
+
 
 
 }
